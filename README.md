26aab6623534d453d7e265f362e4ff82f59e46f0
Juan Felipe Jimenez P <pipesjimenez6@gmail.com>
2022-03-02 13:56:53 -0500

    agregando el carrito de compras

---
+++ README.md
@@ -0,0 +1,2 @@
+# tienda-virtual, this is a example about how to create a website using boostrap, etc. for chemestry: Nuevas tecnologias de desarrollo
+Website: https://juanfelipex.github.io/tienda-virtual/
--- js/customers.js
+++ js/customers.js
@@ -3,8 +3,15 @@ alert("hello world")
 function agregarProducto(id){
     var  product = document.getElementById(id);
     var productName = product.children[0].textContent; 
-    var price = product.children[1].textContent; 
+    var price = parseInt(product.children[1].textContent); 
+    var total = document.getElementById("total").innerText.split(" ");
+    var suma = price + parseInt(total[2]);
+    alert(suma);
     //console.log("Id: " + id  + " Product name: " + productName + " Price: " + price );
+    var ptotal = document.getElementById('total').textContent.replace(total, suma);
+    var cadena = ptotal.replace(total[2], suma);
+    document.getElementById('total').textContent = cadena;
+    alert(cadena)
 
     var item = document.createElement('li')
     item.innerText = productName;
